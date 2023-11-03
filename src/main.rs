c7fdcc425289fecbae1868c61bd64e45eab3c6bd
benzlokzik <121052717+benzlokzik@users.noreply.github.com>
2023-11-03 17:47:38 +0300

    whole code of program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use std::error::Error;
+use std::process;
+use std::time::Duration;
+use structopt::StructOpt;
+use tokio;
+
+#[derive(StructOpt)]
+struct Cli {
+    /// Интервал в секундах
+    interval: u64,
+    /// URL для проверки
+    url: String,
+}
+
+#[tokio::main]
+async fn main() {
+    if let Err(e) = run().await {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+async fn run() -> Result<(), Box<dyn Error>> {
+    let args = Cli::from_args();
+
+    let url = match reqwest::Url::parse(&args.url) {
+        Ok(u) => u,
+        Err(_) => {
+            println!("URL parsing error");
+            return Ok(());
+        }
+    };
+
+    let client = reqwest::Client::new();
+
+    loop {
+        let response = client.get(url.as_str()).send().await;
+
+        match response {
+            Ok(resp) => {
+                if resp.status().is_success() {
+                    println!("Checking '{}'. Result: OK(200)", url);
+                } else {
+                    println!("Checking '{}'. Result: ERR({})", url, resp.status().as_u16());
+                }
+            }
+            Err(e) => {
+                println!("Error occurred while making the HTTP request: {}", e);
+                break;
+            }
+        }
+
+        tokio::time::sleep(Duration::from_secs(args.interval)).await;
+    }
+
+    Ok(())
 }
